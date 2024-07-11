c645869b01130dbe58666a54150d9f6c0e733525
kponky <ikponkedet@gmail.com>
2024-07-11 19:24:27 +0100

    search icon readjusted

--- src/Components/ItemCardHome2.jsx
+++ src/Components/ItemCardHome2.jsx
@@ -5,7 +5,7 @@ import { faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
 import "../styles/itemCard2.css";
 
 const ItemCardHome2 = ({ item }) => {
-  const { addToCart, cartItems } = useCart();
+  const { addToCart, removeFromCart, cartItems } = useCart();
   const [isInCart, setIsInCart] = useState(false);
 
   useEffect(() => {
@@ -17,42 +17,111 @@ const ItemCardHome2 = ({ item }) => {
     }
   }, [cartItems, item.id]);
 
-  const handleAddToCart = () => {
-    addToCart(item, item.id);
-    setIsInCart(true);
-  };
-
-  const handleRemoveFromCart = () => {
-    removeFromCart(item.id);
-    setIsInCart(false);
+  const handleToggleCart = () => {
+    if (isInCart) {
+      removeFromCart(item.id);
+    } else {
+      addToCart(item, item.id);
+    }
+    setIsInCart(!isInCart);
   };
 
   return (
     <div className="card-item__container">
       <div className="cardItem-img">
-      <img src={item.image} alt={item.title} className="itemImg" />
+        <img src={item.image} alt={item.title} className="itemImg" />
       </div>
-      <div clcassName="cardItem-desc">
-      {item.recomm && <span>Recommended </span>}
-      <h4>{item.title}</h4>
-      <p>{item.desc}</p>
-      <div className="itemcard-price-btn">
-        <p>{item.price}</p>
-        {isInCart ? (
-          <button className="btn btn2" onClick={handleRemoveFromCart}>
-            <FontAwesomeIcon icon={faTimes} className="icon" />
+      <div className="cardItem-desc">
+        {item.recomm && <span>Recommended </span>}
+        <h4>{item.title}</h4>
+        <p>{item.desc}</p>
+        <div className="itemcard-price-btn">
+          <p>{item.price}</p>
+          <button className={`btn ${isInCart ? 'btn2' : ''}`} onClick={handleToggleCart}>
+            <FontAwesomeIcon icon={isInCart ? faTimes : faPlus} className="icon" />
           </button>
-        ) : (
-          <button className="btn" onClick={handleAddToCart}>
-            <FontAwesomeIcon icon={faPlus} className="icon" />
-          </button>
-        )}
+        </div>
       </div>
     </div>
-    </div>
-
-
   );
 };
 
 export default ItemCardHome2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from "react";
+// import { useCart } from "../context/CartContext";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
+// import "../styles/itemCard2.css";
+
+// const ItemCardHome2 = ({ item }) => {
+//   const { addToCart, cartItems } = useCart();
+//   const [isInCart, setIsInCart] = useState(false);
+
+//   useEffect(() => {
+//     if (cartItems) {
+//       const isItemInCart = cartItems.some(
+//         (cartItem) => cartItem.id === item.id
+//       );
+//       setIsInCart(isItemInCart);
+//     }
+//   }, [cartItems, item.id]);
+
+//   const handleAddToCart = () => {
+//     addToCart(item, item.id);
+//     setIsInCart(true);
+//   };
+
+//   const handleRemoveFromCart = () => {
+//     removeFromCart(item.id);
+//     setIsInCart(false);
+//   };
+
+//   return (
+//     <div className="card-item__container">
+//       <div className="cardItem-img">
+//       <img src={item.image} alt={item.title} className="itemImg" />
+//       </div>
+//       <div clcassName="cardItem-desc">
+//       {item.recomm && <span>Recommended </span>}
+//       <h4>{item.title}</h4>
+//       <p>{item.desc}</p>
+//       <div className="itemcard-price-btn">
+//         <p>{item.price}</p>
+//         {isInCart ? (
+//           <button className="btn btn2" onClick={handleRemoveFromCart}>
+//             <FontAwesomeIcon icon={faTimes} className="icon" />
+//           </button>
+//         ) : (
+//           <button className="btn" onClick={handleAddToCart}>
+//             <FontAwesomeIcon icon={faPlus} className="icon" />
+//           </button>
+//         )}
+//       </div>
+//     </div>
+//     </div>
+
+
+//   );
+// };
+
+// export default ItemCardHome2;
+
